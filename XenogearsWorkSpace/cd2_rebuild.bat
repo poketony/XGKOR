c6c38d10477d1e56c13b9442a9d9a6e660b53cf0
poketony <57011422+poketony@users.noreply.github.com>
2026-03-01 13:35:06 +0900

    20260301 빌드
    -스토리 번역 및 파일 적용 완료(디버깅 룸은 제외)
    -폰트 추가: 캬/뮤/캠/춥/좇/텄/갉/컫
    -세파르디 컷신 오류 현상을 영판 파일을 유용하여 해결
    -빌리 구식총/고식총 표기 통일(구식 총으로)
    -아니마의 던전 1 진입 전까지 줄조정 완료
    -스크립트에서 ……,/~, 표현을 일괄 수정함
    -DISC 1/2 빌드 환경을 재조정

--- XenogearsWorkSpace/cd2_rebuild.bat
+++ XenogearsWorkSpace/cd2_rebuild.bat
@@ -1,2 +1,22 @@
+@echo off
+
+REM 현재 위치 저장
+set ORIG_DIR=%cd%
+
+REM 파일 이동
+move "%ORIG_DIR%\0022.rootexecd2" "%ORIG_DIR%\STRIPCD1(KOR)\"
+
+REM ===== 여기서 작업 수행 =====
+REM 예: 프로그램 실행
 xenoiso cd2list.txt -d
+
+REM ============================
+
+REM 작업 끝나면 파일 다시 원위치로
+move "%ORIG_DIR%\STRIPCD1(KOR)\0022.rootexecd2" "%ORIG_DIR%\"
+
+REM 원래 위치로 복귀
+cd /d "%ORIG_DIR%"
+
+echo DISC 2 builded
 pause
